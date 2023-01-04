6a25fa300da1d215272ac9cd687e5f717545ca8b
gary <gary@garydeMacBook-Pro.local>
2023-01-04 20:31:58 +0800

    init

---
+++ mods/copy-paste.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#|==============================================================|
+#|              !!!!!!DON'T EDIT THIS FILE!!!!!!                |
+#|  This file should be generated automatically without user i- |
+#|  ntervention (unless you are editing the template)           |
+#|==============================================================|
+
+cp $mod_from $mod_to
